ff2caeb1fa9d68d2d9a33974b60e5718aeccb972
katehjd21 <katedrew2103@hotmail.co.uk>
2025-12-06 08:09:24 +0000

    changing dynamodb table name

--- backend.tf
+++ backend.tf
@@ -3,7 +3,7 @@ terraform {
     bucket      = "kd-hard-to-heat-homes-s3"
     key         = "hard-to-heat-homes-2.0/terraform.tfstate"
     region      = "eu-west-2"
-    dynamodb_table = "kd-h2hstate-table"
+    dynamodb_table = "kd-h2h-state-table"
     encrypt     = true
   }
 }
