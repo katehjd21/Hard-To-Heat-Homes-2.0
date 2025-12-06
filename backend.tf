4d48132dbe5fa6314c6c988f6ed88b30cdca861d
katehjd21 <katedrew2103@hotmail.co.uk>
2025-12-06 07:32:10 +0000

    adding sg's and vpc to main.tf

--- backend.tf
+++ backend.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     bucket      = "hard-to-heat-homes-s3"
     key         = "hard-to-heat-homes-2.0/terraform.tfstate"
-    region      = "eu-north-1"
+    region      = "eu-west-2"
     dynamodb_table = "state-table"
     encrypt     = true
   }
--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 provider "aws" {
-  region = "eu-north-1"
+  region = "eu-west-2"
 }
 
 data "aws_ami" "ubuntu" {
@@ -44,22 +44,65 @@ resource "local_file" "public_key" {
   filename = "./.ssh/terraform_rsa.pub"
 }
 
-resource "aws_instance" "app_server" {
-  ami                    = data.aws_ami.ubuntu.id
-  instance_type          = "t3.small"
-  subnet_id              = "subnet-84e2f4fc"
-  vpc_security_group_ids = ["sg-00d5a1d67232f43a4"]
-  key_name               = "terraform-key"
-  user_data = templatefile("${path.module}/cloud-init.yaml.tmpl", {
-    instance_name = "hard-to-heat-homes-2.0"
-    epc_api_key   = var.epc_api_key
-    os_api_key    = var.os_api_key
-    session_secret_key = var.session_secret_key
-  })
+resource "aws_vpc" "sandbox_vpc" {
+  cidr_block = "10.0.0.0/16"
+  tags = { Name = "sandbox-vpc" }
+}
 
+resource "aws_subnet" "sandbox_subnet" {
+  vpc_id                  = aws_vpc.sandbox_vpc.id
+  cidr_block              = "10.0.1.0/24"
+  availability_zone       = "eu-west-2a"
+  map_public_ip_on_launch = true
+}
 
-  tags = {
-    Name = "hard-to-heat-homes-2.0"
+resource "aws_security_group" "app_sg" {
+  name        = "app-sg"
+  description = "Allow SSH, HTTP, and App"
+  vpc_id      = aws_vpc.sandbox_vpc.id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 }
 
+resource "aws_instance" "app_server" {
+  ami                    = data.aws_ami.ubuntu.id
+  instance_type          = "t3.small"
+  subnet_id              = aws_subnet.sandbox_subnet.id
+  vpc_security_group_ids = [aws_security_group.app_sg.id]
+  key_name               = "terraform-key"
+
+  user_data = templatefile("${path.module}/cloud-init.yaml.tmpl", {
+    instance_name       = "hard-to-heat-homes-2.0"
+    epc_api_key         = var.epc_api_key
+    os_api_key          = var.os_api_key
+    session_secret_key  = var.session_secret_key
+  })
+
+  tags = { Name = "hard-to-heat-homes-2.0" }
+}
